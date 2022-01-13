08fe323aa624311a7526c895b5e18bda4eef0893
Marseek <x-bit89@mail.ru>
2022-01-13 16:22:49 +0300

    delete unnesessary files

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE states(
-                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                         application TEXT,
-                         param1 TEXT,
-                         param2 INTEGER,
-                         version INTEGER
+CREATE TABLE states (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        application TEXT,
+                        param1 TEXT,
+                        param2 INTEGER,
+                        version INTEGER
 );
